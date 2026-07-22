39ed5fd7ef222726289656c65908d63c5b632dd9
Dublonx <dublonjmx17@gmail.com>
2026-07-22 14:38:45 +0800

    Add main.js for functionality

---
+++ main.js
@@ -0,0 +1,8 @@
+// certifications preview
+const overlay = document.querySelector('#overlay');
+const imageOverlay = document.querySelector('#imageOverlay img');
+const prev = document.querySelectorAll('.prev');
+
+prev.forEach(img => {
+    console.log("Clicked!");
+});
