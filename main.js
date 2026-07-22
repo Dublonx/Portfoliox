eacb206e0d5978536051c40de1a2ce32fb55c82a
Dublonx <dublonjmx17@gmail.com>
2026-07-22 15:36:02 +0800

    Add all changes

--- main.js
+++ main.js
@@ -2,15 +2,18 @@
 const overlay = document.querySelector('#overlay');
 const previewImg = document.querySelector('#preview-img');
 const imgCert = document.querySelectorAll('.img-cert');
+const body = document.body;
 
 imgCert.forEach(img => {
     img.onclick = () => {
         overlay.classList.add("active");
         previewImg.src = img.src;
+        body.classList.add("no-scroll");
     };
 });
 
 // close certifications preview
 overlay.onclick = () => {
     overlay.classList.remove("active");
+    body.classList.remove("no-scroll");
 }
